5f6c195ceb60beda01394ba76238c301c8d8544c
AustinS23 <austin.shen@pushpay.com>
2021-01-26 16:28:38 +1300

    Add Card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,5 @@
+const Card = (props: any) => {
+    return <div className="bg-white w-100 shadow-lg p-4" {...props} />;
+};
+
+export default Card;
--- src/pages/overview/top-music/index.tsx
+++ src/pages/overview/top-music/index.tsx
@@ -1,7 +1,10 @@
+import Card from "../../../components/card"
+
 const TopMusic = () => {
     return (
         <section>
-            <h3>Top Music</h3>
+            <h3 className="mb-5">Top Music</h3>
+            <Card>TODO</Card>
         </section>
     );
 }
